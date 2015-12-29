3abcdb547c854f6715931dac51b3e766be071e9e
PoLin <showoowohs@gmail.com>
2015-12-29 14:11:53 +0800

    modify ./studio/PoAidl/ReadMe.mk, add 參考資料

--- studio/PoAidl/ReadMe.mk
+++ studio/PoAidl/ReadMe.mk
@@ -16,3 +16,9 @@ OS: android 4.2.2
 
 ### maintain
 > 2015/12/29 add
+
+### reference
+[1. aminzai github](https://github.com/aminzai/Android-AIDL-Sample)
+[2. 觀念介紹](http://jjnnykimo.pixnet.net/blog/post/31622824-aidl-----android%E4%B8%AD%E7%9A%84%E9%81%A0%E7%A8%8B%E6%8E%A5%E5%8F%A3)
+[3. ](http://chaos1209.blogspot.tw/2012/12/android-aidl.html)
+[4. ](https://wefollownews.appspot.com/cittopnews201408_10/12510.html)
