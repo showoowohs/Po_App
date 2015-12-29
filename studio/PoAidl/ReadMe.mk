d7937a9f1eeef5fc6c88cf453dce4a8948fd79cc
PoLin <showoowohs@gmail.com>
2015-12-29 13:46:56 +0800

    add ReadMe.mk

---
+++ studio/PoAidl/ReadMe.mk
@@ -0,0 +1,18 @@
+### Test Environment
+Devices: AT302
+OS: android 4.2.2
+---
+
+### Remark:
+>This APP is a AIDL sample
+
+---
+
+### result picture:
+> 1. result
+![alt tag](https://.../dome/loop_thermal.gif)
+
+---
+
+### maintain
+> 2015/12/29 add
